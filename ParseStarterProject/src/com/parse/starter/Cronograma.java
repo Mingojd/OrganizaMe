15b3d09508a534580b0a9426f2a01a86694a7e75
josed <jose.domingo.guzman@gmail.com>
2013-05-21 10:34:05 -0600

    c

--- ParseStarterProject/src/com/parse/starter/Cronograma.java
+++ ParseStarterProject/src/com/parse/starter/Cronograma.java
@@ -17,6 +17,7 @@ public class Cronograma extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.activity_cronograma, menu);
 		return true;
+		//prueba github
 	}
 
 }
